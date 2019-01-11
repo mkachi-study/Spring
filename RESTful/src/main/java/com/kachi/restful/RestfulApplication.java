b2e43fa77f92d10cf83bba3b35ec1b8cba109eee
MKachi <mkachi@naver.com>
2019-01-11 17:58:27 +0900

    Update RESTful project

---
+++ RESTful/src/main/java/com/kachi/restful/RestfulApplication.java
@@ -0,0 +1,14 @@
+package com.kachi.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulApplication.class, args);
+    }
+
+}
+
---
+++ RESTful/src/test/java/com/kachi/restful/RestfulApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kachi.restful;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
